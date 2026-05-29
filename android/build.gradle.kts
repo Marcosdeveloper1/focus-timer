bdb043a5c8b40edaecc88f57a71e93ff15ef3ea5
Marcos Levi <163767549+Marcosdeveloper1@users.noreply.github.com>
2026-05-29 10:32:51 -0300

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,60 +1,6 @@
-import java.util.Properties
-
-plugins {
-    id("com.android.application")
-    id("kotlin-android")
-    id("dev.flutter.flutter-gradle-plugin")
-}
-
-val keyProperties = Properties()
-val keyPropertiesFile = rootProject.file("key.properties")
-if (keyPropertiesFile.exists()) {
-    keyProperties.load(keyPropertiesFile.inputStream())
-}
-
-android {
-    namespace = "com.fala.geek"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
-
-    compileOptions {
-        isCoreLibraryDesugaringEnabled = true
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
     }
-
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
-    }
-
-    signingConfigs {
-        create("release") {
-            keyAlias = keyProperties["keyAlias"] as String? ?: "focus-timer"
-            keyPassword = keyProperties["keyPassword"] as String? ?: "FalaGeek2026"
-            storeFile = file(keyProperties["storeFile"] as String? ?: "focus-timer.jks")
-            storePassword = keyProperties["storePassword"] as String? ?: "FalaGeek2026"
-        }
-    }
-
-    defaultConfig {
-        applicationId = "com.fala.geek"
-        minSdk = 21
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
-    }
-
-    buildTypes {
-        release {
-            signingConfig = signingConfigs.getByName("release")
-        }
-    }
-}
-
-flutter {
-    source = "../.."
-}
-
-dependencies {
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 }
