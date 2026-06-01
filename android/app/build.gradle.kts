a1e8a35ad7d89c1397df9bacd8556b7023b06980
Marcos Levi <163767549+Marcosdeveloper1@users.noreply.github.com>
2026-06-01 07:20:14 -0300

    Enable minification for release build type

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -48,6 +48,11 @@ android {
     buildTypes {
         release {
             signingConfig = signingConfigs.getByName("release")
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
 }
